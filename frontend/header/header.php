fc065528162d46f408ddb60d700bfe7a2cbcbda8
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-03-14 13:24:41 +0800

    css home

--- frontend/header/header.php
+++ frontend/header/header.php
@@ -1,10 +1,19 @@
+<?php
+
+$current_page = basename($_SERVER['REQUEST_URI'], ".php");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../header/header.css"> 
     <script defer src="script.js"></script> 
 </head>
@@ -13,26 +22,26 @@
 <header>
     <nav class="navbar">
         <div class="logo">
-            <a href="index.php">
+            <a href="../home/home.php">
                 <img src="../images/SEC_LOGO.png" alt="Logo">
             </a>
         </div>
 
         <!-- Navigation Links -->
         <ul class="nav-links">
-            <li><a href="index.php" class="">Home</a></li>
-            <li><a href="article.php">Article</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="contact.php">Contact Us</a></li>
+            <li><a href="../home/home.php" class="<?= ($current_page == 'home') ? 'active' : '' ?>">Home</a></li>
+            <li><a href="../article/article.php" class="<?= ($current_page == 'article') ? 'active' : '' ?>">Article</a></li>
+            <li><a href="../about/about.php" class="<?= ($current_page == 'about') ? 'active' : '' ?>">About</a></li>
+            <li><a href="../contact/contact.php" class="<?= ($current_page == 'contact') ? 'active' : '' ?>">Contact Us</a></li>
         </ul>
 
         <!-- Right-side Icons & Buttons -->
         <div class="nav-right">
             <a href="faqs.php" class="btn-faq">FAQs</a>
-            <button class="icon-btn"><img src="user-icon.png" alt="User"></button>
+            <button class="user-btn"><i class="fa-solid fa-circle-user"></i></button>
             <div class="dropdown">
-                <button class="icon-btn settings-btn">
-                    <img src="settings-icon.png" alt="Settings">
+                <button class="setting-btn">
+                    <i class="fa-solid fa-gear"></i>
                 </button>
                 <div class="dropdown-content">
                     <p>Appearance</p>
--- frontend/home/home.php
+++ frontend/home/home.php
@@ -5,8 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="home.css">   
     <title>HOME | SEC</title>
+    <main class="maincontent-page">
+        <h1><span>SEC</span>pedia</h1>
+        <p>Welcome to SECpedia, the collaborative platform where you can learn, contribute, and share knowledge about business registration, compliance, investor protection, and SEC regulations. </p>
+    </main>
+    
+    
 </head>
 <body>
 
