53713e5b054ecf9f3d250b34b6c3442b03f6d8c5
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-03-17 16:44:40 +0800

    LOGIN AND REGISTER CLIENT

---
+++ frontend/login/login.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN | SEC </title>
+</head>
+<body>
+    
+    <div class="login-container">
+        <div class="login-form">
+            <h1><span class="SEC">Sec</span>pedia</h1>
+        </div>
+
+    </div>
+
+</body>
+</html>
